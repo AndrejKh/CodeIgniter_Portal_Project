ef73805f567ede2203f692cbe19703a928b64b53
AndrejKh <silverwewel@gmail.com>
2015-05-19 15:05:30 +0200

    Fixed a typo

--- public/static/js/group-manager.js
+++ public/static/js/group-manager.js
@@ -864,7 +864,7 @@ $(function() {
 			// Generic {{{
 
 			$(document).ajaxSend(function(e, request, settings) {
-				// Append a CSRF token to all ajax POST requets.
+				// Append a CSRF token to all AJAX POST requests.
 				if (settings.type === 'POST' && settings.data.length)
 					settings.data
 						+= '&' + encodeURIComponent(YodaPortal.csrf.tokenName)
