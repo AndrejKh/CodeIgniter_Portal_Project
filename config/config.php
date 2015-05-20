4d31b89edbcfa2aec7c23614c39f3bf224269530
AndrejKh <silverwewel@gmail.com>
2015-05-20 09:19:50 +0200

    Fixed a doc typo

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-// Yoda Portal Module Group Manager onfiguration {{{
+// Yoda Portal Module Group Manager configuration {{{
 
 // No module-specific config yet.
 $config = array();
