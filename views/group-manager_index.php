2c8a96b57185b781b2938afd1a54be9d59a7e2c1
AndrejKh <silverwewel@gmail.com>
2020-12-01 11:15:01 +0100

    Fix typo.

--- views/group-manager_index.php
+++ views/group-manager_index.php
@@ -271,7 +271,7 @@ $(function() {
 	<div class="modal-dialog">
 		<div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title">Confirm user removall</h5>
+              <h5 class="modal-title">Confirm user removal</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
